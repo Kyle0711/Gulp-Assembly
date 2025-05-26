c36693cde31792a1056430d50958d4313ff57e2c
RomKo755 <romankovalchuk9182@gmail.com>
2025-05-26 19:29:29 +0300

    first commit

---
+++ gulp/tasks/images.js
@@ -0,0 +1,16 @@
+import webp from 'gulp-webp'
+import imagemin from 'gulp-imagemin'
+
+export const images = () => {
+    return app.gulp.src(app.path.src.images)
+        .pipe(app.plugins.newer(app.path.build.images))
+        .pipe(app.plugins.if(app.isBuild, webp()))
+        .pipe(app.plugins.if(app.isBuild, app.gulp.dest(app.path.build.images)))
+        .pipe(app.plugins.if(app.isBuild, app.gulp.src(app.path.src.images)))
+        .pipe(app.plugins.if(app.isBuild, app.plugins.newer(app.path.build.images)))
+        .pipe(app.plugins.if(app.isBuild, imagemin({})))
+        .pipe(app.gulp.dest(app.path.build.images))
+        .pipe(app.gulp.src(app.path.src.svg))
+        .pipe(app.gulp.dest(app.path.build.images))
+        .pipe(app.plugins.browserSync.stream())
+}
